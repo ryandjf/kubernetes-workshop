46d58616b0ca222b87204714f6f312af7d30454b
Junfeng Dai <ryandjf@hotmail.com>
2019-02-09 15:21:05 +0800

    remove tunnel

--- jenkins/slave/slave.py
+++ jenkins/slave/slave.py
@@ -41,9 +41,6 @@ def slave_run(slave_jar, jnlp_url):
     params = [ 'java', '-jar', slave_jar, '-jnlpUrl', jnlp_url ]
     if os.environ['JENKINS_SLAVE_ADDRESS'] != '':
         params.extend([ '-connectTo', os.environ['JENKINS_SLAVE_ADDRESS' ] ])
-    
-    if os.environ['JENKINS_TUNNEL'] != '':
-        params.extend([ '-jnlpTunnel', os.environ['JENKINS_TUNNEL' ] ])
 
     if os.environ['SLAVE_SECRET'] == '':
         params.extend([ '-jnlpCredentials', os.environ['JENKINS_USER'] + ':' + os.environ['JENKINS_PASS'] ])
